db88b24029284bc8184fa30e178594049423b8f5
hazycora <hazysu@riseup.net>
2023-08-02 18:10:47 -0500

    lint

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ import satori from 'satori'
 
 await initWasm(fs.readFileSync('node_modules/@resvg/resvg-wasm/index_bg.wasm'))
 
-function typedarrayToBuffer(arr) {
-	return ArrayBuffer.isView(arr)
+function typedarrayToBuffer(array) {
+	return ArrayBuffer.isView(array)
 		? // To avoid a copy, use the typed array's underlying ArrayBuffer to back
 		  // new Buffer, respecting the "view", i.e. byteOffset and byteLength
-		  Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength)
+		  Buffer.from(array.buffer, array.byteOffset, array.byteLength)
 		: // Pass through all other types to `Buffer.from`
-		  Buffer.from(arr)
+		  Buffer.from(array)
 }
 
 function styleStringToObject(style) {
+	if (!style) return {}
 	return Object.fromEntries(
 		style
 			.split(';')
-			.filter(string => string)
+			.filter(Boolean)
 			.map(rule => {
 				const [name, value] = rule.trim().split(':')
 				const jsName = name
 					.split('-')
-					.map((e, i) => {
-						if (i == 0) return e
-						return e[0].toUpperCase() + e.slice(1)
+					.map((element, index) => {
+						if (index == 0) return element
+						return element[0].toUpperCase() + element.slice(1)
 					})
 					.join('')
 				return [jsName.trim(), value.trim()]
@@ -37,7 +38,7 @@ function parseElement(element) {
 	if (element.nodeType != 1) {
 		return element._rawText
 	}
-	let result = {
+	const result = {
 		type: element.rawTagName,
 		props: {
 			...element.attributes,
@@ -45,15 +46,18 @@ function parseElement(element) {
 		}
 	}
 	if (element.childNodes.length > 0) {
-		result.props.children = element.childNodes.map(parseElement)
+		result.props.children = element.childNodes.map(element =>
+			parseElement(element)
+		)
 	}
 	return result
 }
 
 function parseCSS(css) {
-	const ruleRegexp = /\s*((?:[\.#]?[a-zA-Z0-:9 >]+|\*))\s*\{(.*?)\}\s*/s
+	const ruleRegexp = /\s*((?:[\d #.:>A-Za-z]+|\*))\s*{(.*?)}\s*/s
 	const rules = []
 	while (css.length > 0) {
+		console.log('ok', css)
 		const selector = ruleRegexp.exec(css)[1].trim()
 		let properties = ruleRegexp.exec(css)[2].trim()
 		if (properties.endsWith(';')) properties = properties.slice(0, -1)
@@ -63,23 +67,20 @@ function parseCSS(css) {
 	return Object.fromEntries(rules)
 }
 
-export default async function render(
-	html,
-	css,
-	options
-) {
+export default async function render(html, css, options) {
 	options = options ?? {}
 	if (!options.width) options.width = 1200
 	if (!options.height) options.height = 600
 	const parsedCss = parseCSS(css)
 	const root = parse(html)
-	for (let key of Object.keys(parsedCss)) {
+	for (const key of Object.keys(parsedCss)) {
 		const classRules = parsedCss[key]
-		for (let elem of root.querySelectorAll(key)) {
-			elem.setAttribute(
-				'style',
-				(elem.getAttribute('style') ?? '') + classRules.trim()
-			)
+		for (const element of root.querySelectorAll(key)) {
+			let existingStyle = element.getAttribute('style')?.trim() ?? ''
+			if (existingStyle != '' && !existingStyle.endsWith(';')) {
+				existingStyle += ';'
+			}
+			element.setAttribute('style', existingStyle + classRules.trim())
 		}
 	}
 	const htmlObjects = parseElement(root.firstChild)
