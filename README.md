ba23cc0a03fbf25f70b14cc3ecb7a72a2aa2a5e6
hazycora <hazysu@riseup.net>
2023-08-02 02:07:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# @besties/html2png
+
+```js
+import html2png from '@besties/html2png'
+
+fs.promises.writeFile(
+	'test.png',
+	await html2png(
+		`<div class="main">
+			<div class="contents">
+				<div class="info">
+					<p style="font-size: 20px; color: #9A8FA7;">@besties/html2png</p>
+					<p style="font-weight: 800; font-size: 40px;">wow! a very cool example!</p>
+				</div>
+				<div class="graphics">
+					<img width="120" height="120" src="https://git.gay/avatars/d2d033ed83e7ef19a0f279e6cc9e32c3f3563504ec2aa6af1893f376b2074ffb?size=120"></img>
+				</div>
+			</div>
+		</div>`,
+		`p {
+			margin: 0;
+		}
+
+		.main {
+			color: #ffffff;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: #1B171F;
+			width: 1200px;
+			height: 600px;
+		}
+
+		.contents {
+			display: flex;
+		}
+
+		.info {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			width: 800px;
+			gap: 10px;
+		}
+
+		.graphics {
+			display: flex;
+			justify-content: flex-end;
+			align-items: flex-start;
+			width: 240px;
+		}
+
+		.graphics > img {
+			border-radius: 100%;
+			width: 120px;
+			height: 120px;
+		}`
+	)
+)
+```
+
+made with ❤️ by besties
---
+++ index.js
@@ -0,0 +1,114 @@
+import { parse } from 'node-html-parser'
+import { Resvg, initWasm } from '@resvg/resvg-wasm'
+import fs from 'node:fs'
+import satori from 'satori'
+
+await initWasm(fs.readFileSync('node_modules/@resvg/resvg-wasm/index_bg.wasm'))
+
+const figtreeRegular = fs.readFileSync('./fonts/Figtree-Regular.woff')
+const figtreeMedium = fs.readFileSync('./fonts/Figtree-Medium.woff')
+const figtreeExtraBold = fs.readFileSync('./fonts/Figtree-ExtraBold.woff')
+
+function typedarrayToBuffer(arr) {
+	return ArrayBuffer.isView(arr)
+		? // To avoid a copy, use the typed array's underlying ArrayBuffer to back
+		  // new Buffer, respecting the "view", i.e. byteOffset and byteLength
+		  Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength)
+		: // Pass through all other types to `Buffer.from`
+		  Buffer.from(arr)
+}
+
+function styleStringToObject(style) {
+	return Object.fromEntries(
+		style
+			.split(';')
+			.filter(string => string)
+			.map(rule => {
+				const [name, value] = rule.trim().split(':')
+				const jsName = name
+					.split('-')
+					.map((e, i) => {
+						if (i == 0) return e
+						return e[0].toUpperCase() + e.slice(1)
+					})
+					.join('')
+				return [jsName.trim(), value.trim()]
+			})
+	)
+}
+
+function parseElement(element) {
+	if (element.nodeType != 1) {
+		return element._rawText
+	}
+	let result = {
+		type: element.rawTagName,
+		props: {
+			...element.attributes,
+			style: styleStringToObject(element.getAttribute('style'))
+		}
+	}
+	if (element.childNodes.length > 0) {
+		result.props.children = element.childNodes.map(parseElement)
+	}
+	return result
+}
+
+function parseCSS(css) {
+	const ruleRegexp = /\s*((?:[\.#]?[a-zA-Z0-:9 >]+|\*))\s*\{(.*?)\}\s*/s
+	const rules = []
+	while (css.length > 0) {
+		const selector = ruleRegexp.exec(css)[1].trim()
+		let properties = ruleRegexp.exec(css)[2].trim()
+		if (properties.endsWith(';')) properties = properties.slice(0, -1)
+		rules.push([selector, properties])
+		css = css.replace(ruleRegexp, '').trim()
+	}
+	return Object.fromEntries(rules)
+}
+
+export default async function render(
+	html,
+	css,
+	options = { width: 1200, height: 600 }
+) {
+	const parsedCss = parseCSS(css)
+	const root = parse(html)
+	for (let key of Object.keys(parsedCss)) {
+		const classRules = parsedCss[key]
+		for (let elem of root.querySelectorAll(key)) {
+			elem.setAttribute(
+				'style',
+				(elem.getAttribute('style') ?? '') + classRules.trim()
+			)
+		}
+	}
+	const htmlObjects = parseElement(root.firstChild)
+	const svg = await satori(htmlObjects, {
+		width: options.width,
+		height: options.height,
+		fonts: options.fonts ?? [
+			{
+				name: 'Figtree',
+				data: figtreeRegular,
+				weight: 400,
+				style: 'normal'
+			},
+			{
+				name: 'Figtree',
+				data: figtreeMedium,
+				weight: 500,
+				style: 'normal'
+			},
+			{
+				name: 'Figtree',
+				data: figtreeExtraBold,
+				weight: 800,
+				style: 'normal'
+			}
+		]
+	})
+	const resvgJs = new Resvg(svg)
+	const pngBuffer = resvgJs.render().asPng()
+	return typedarrayToBuffer(pngBuffer)
+}
